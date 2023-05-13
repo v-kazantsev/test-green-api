c235f402661088015ebcb00915d8a9020aa5569a
v-kazantsev <vvs201@mail.ru>
2023-05-13 16:15:09 +0700

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import * as path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3001,
+    open: true
+  },
+  plugins: [
+    react(),
+    tsconfigPaths(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/scss')
+    }
+  }
+})
