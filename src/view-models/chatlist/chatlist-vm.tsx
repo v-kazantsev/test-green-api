d92ef59df09382e2e2e7a18801807fd0433c6f79
v-kazantsev <vvs201@mail.ru>
2023-05-17 14:32:37 +0700

    add check for persisted contacts

--- src/view-models/chatlist/chatlist-vm.tsx
+++ src/view-models/chatlist/chatlist-vm.tsx
@@ -1,12 +1,15 @@
 import { useEffect } from 'react';
 import { useAppDispatch } from '@/hooks/use-app-dispatch';
+import { useAppSelector } from '@/hooks/use-app-selector';
 import { getContacts } from '@/features/contacts/actions/get-contacts';
+import { contactsSelector } from '@/features/contacts/selectors/contacts-selector';
 import { ChatListView } from '@/views';
 
 export const ChatListVM = () => {
   const dispatch = useAppDispatch();
+  const contacts = useAppSelector(contactsSelector);
   useEffect(() => {
-    dispatch(getContacts());
-  }, [dispatch]);
+    if (contacts?.length === 0) dispatch(getContacts());
+  }, [dispatch, contacts]);
   return <ChatListView />
-}
+};
